fde867ac4e9ebe6b68e122dd0cbd009a5901e879
JphanACC <69232559+JphanACC@users.noreply.github.com>
2021-08-13 15:32:09 -0600

    new updates

--- src/main/java/com/revature/models/Invoice.java
+++ src/main/java/com/revature/models/Invoice.java
@@ -27,7 +27,6 @@ public class Invoice {
 	
 	@Column(name="native_amount")
 	private double nativeAmount;
-<<<<<<< HEAD
 	
 	@Column(name="usd_amount")
 	private double usdAmount;
@@ -36,9 +35,6 @@ public class Invoice {
 	@JoinColumn
 	@JsonBackReference
 	private User user;
-=======
-
->>>>>>> bd9b07a14cf0f42547550241b7fb48e79d1b8aff
 
 	public Invoice() {
 		super();
