6379c64342a0d799090a526162c419228d8295e8
DannyLGZ <linggezeng@gmail.com>
2021-08-15 15:26:43 -0400

    update url

--- src/main/java/com/revature/repos/InvoiceDAOImpl.java
+++ src/main/java/com/revature/repos/InvoiceDAOImpl.java
@@ -1,10 +1,12 @@
 package com.revature.repos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import com.revature.models.Book;
 import com.revature.models.Invoice;
 import com.revature.models.Order;
 import com.revature.models.User;
+
 
 
 @Repository
@@ -33,21 +36,23 @@ public class InvoiceDAOImpl implements InvoiceDAO{
 	}
 
 	
+	
 	@Override
-	public void addInvoice(User user, Book book, double nativeAmount, int quantity) {
+	public void addInvoice(User user, Book book, double nativeAmount, String name) {
 		Session session = sessionFactory.getCurrentSession();
 		Invoice invoice = new Invoice();
 		invoice.setInvoiceStatus("new");
-		invoice.setNativeCurrency(user.getCurrencyID());
+		invoice.setNativeCurrency(name);
 		invoice.setNativeAmount(nativeAmount);
 		invoice.setUser(user);
-		double usdTotal = book.getCostUSD() *quantity;
+		double usdTotal = book.getCostUSD() * 1;
 		invoice.setUsdAmount(usdTotal);
 		session.save(invoice);
 		log.info("User create a new invoice ticket.");
 	}
 
-	
+
+
 	@Override
 	public List<Invoice> findAllInvoice() {
 		Session session = sessionFactory.getCurrentSession();
@@ -57,6 +62,15 @@ public class InvoiceDAOImpl implements InvoiceDAO{
 //		return session.createQuery(cq).getResultList();
 		return session.createQuery("FROM Invoice").list();
 	}
+
+	
+	@Override
+	public Invoice findInvoiceById(int invoiceId) {
+		Session session = sessionFactory.getCurrentSession();
+		log.info("Admin retrieved an invoice " + invoiceId);
+		return session.get(Invoice.class, invoiceId);
+	}
+	
 	
 	
 	@Override
@@ -76,25 +90,66 @@ public class InvoiceDAOImpl implements InvoiceDAO{
 		return invoice;
 	}
 
-	@Override
-	public Invoice findInvoiceById(int invoiceId) {
-		Session session = sessionFactory.getCurrentSession();
-		log.info("Admin retrieved an invoice " + invoiceId);
-		return session.get(Invoice.class, invoiceId);
-	}
-	
+
 
 	@Override
-	public void addOrder(User user, Book book, Invoice invoice, int quantity) {
+	public void addOrder(User user, Book book, Invoice invoice) {
 		Session session = sessionFactory.getCurrentSession();
 		Order order = new Order();
-		order.setOrderQuantity(quantity);
+		order.setOrderQuantity(1);
 		order.setBook(book);
-		order.setInvoice(invoice);
 		order.setUser(user);
+		order.setInvoice(invoice);
 		session.save(order);
 		log.info("User created a new Order");
 		
 	}
+
+
+	// all orders by id but untested but should work
+	@Override
+	public List<Order>  findOrderByuserId(int userId) {
+		Session session = sessionFactory.getCurrentSession();
+		String sql = " SELECT orders.* "
+				+ "FROM orders "
+				+ "LEFT JOIN invoice ON orders.USER_ID = invoice.USER_ID WHERE orders.USER_ID = :userid ; ";
+		Query<Order> query = session.createNativeQuery(sql, Order.class);
+		query.setParameter("userid", userId);
+		List<Order> list =  query.getResultList();
+		log.info("User getting all orders by id.");
+		return list;
+	}
+	// returns all orders base on userId. Includes pending,deny,approve,cancel/reject
+
+	// this will update the status but untested
+	@Override
+	public void updateStatusbyId(int invoiceId, String status) {
+		Session session = sessionFactory.getCurrentSession();
+		String sql = "SELECT * FROM invoice WHERE INVOICE.INVOICE_ID = :invoiceid;" ;
+		NativeQuery<Invoice> query = session.createNativeQuery(sql, Invoice.class);
+		query.setParameter("invoiceid", invoiceId);
+		List<Invoice> result = query.getResultList();
+		Invoice invoice = (Invoice) result.get(0);
+		invoice.setInvoiceStatus(status);
+		log.info("User update one of his invoice tickets status.");
+		session.merge(invoice);
+		
+		
+	}
+
+	// this method is not tested for admin
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Order> allOrder() {
+		Session session = sessionFactory.getCurrentSession();
+		log.info("Admin found all orders in the database. ");
+		return session.createQuery("FROM Order").list();
+	}
+
+	
+	
+	
+	
+
 
 }
