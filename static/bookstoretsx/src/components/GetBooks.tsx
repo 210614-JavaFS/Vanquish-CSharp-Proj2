b5d5ac26b427568a154e51633815f899b9b50d3b
Munibk22 <Munib22@gmail.com>
2021-08-15 16:21:39 -0400

    switched out api books for database books

--- static/bookstoretsx/src/components/GetBooks.tsx
+++ static/bookstoretsx/src/components/GetBooks.tsx
@@ -4,25 +4,14 @@ import Books from '../models/Books';
 import { BookApiResponse } from '../remote/bookApi';
 import booksClient from '../remote/BooksClient';
 
-// type Props = {
-//     title: string,
-//     rank: string,
-//     primary_isbn13: string,
-//     publisher: string,
-//     description: string,
-//     author: string,
-//     book_image: string,
-//     isbns: [{}]
-// }
-
 type Props = {
     getRate: number
 }
 
 const GetBooks: React.FC<Props> = ({ getRate }) => {
     const [books, setBooks] = useState<[]>([]);
-    const baseURL = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=";
-    const KEY1 = "wfShq9i9bbJXOyQzHxUKg1FCSftgjdjn";
+    const baseURL = "http://localhost:8080/bookstore/admin/allbook";
+
 
     useEffect(() => {
         console.log(getRate);
@@ -31,15 +20,17 @@ const GetBooks: React.FC<Props> = ({ getRate }) => {
         let source = axios.CancelToken.source();
 
         const fetchBooks = async () => {
-            const res = await axios.get(baseURL + KEY1, {
+            const res = await axios.get(baseURL, {
                 cancelToken: source.token
             })
-            // console.log(res.data.results.books);
-            const data = res.data.results.books;
-            setBooks(data);
 
+            if (res != null) {
+                const data = res;
+                console.log(data);
+                console.log(data.data);
+                setBooks(data.data);
+            }
             return () => {
-                // abortController.abort();
                 console.log("Cancelling api call");
                 source.cancel();
             }
@@ -64,24 +55,6 @@ const GetBooks: React.FC<Props> = ({ getRate }) => {
         return { status: 404, message: "Failed to fetch Book" }
     }
 
-    async function getApiBooks() {
-
-        const baseURL2 = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=";
-
-        const KEY = "wfShq9i9bbJXOyQzHxUKg1FCSftgjdjn";
-
-        let response = await fetch(baseURL2 + KEY)
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log(data)
-        //     setBooks(data)
-        // }       // )
-        let data = await response.json();
-        // console.log(data);
-
-
-    }
-
     return (
         <div className="">
             <div className="">
@@ -91,19 +64,25 @@ const GetBooks: React.FC<Props> = ({ getRate }) => {
             <div className="flexwrap justify">
                 {books.map((book) => {
 
-                    const { title,
-                        rank,
-                        primary_isbn13,
-                        publisher,
-                        description,
+                    const {
+                        bookId,
                         author,
-                        book_image,
-                        isbns } = book
+                        bookCategory,
+                        bookDescription,
+                        bookName,
+                        bookStockQuantity,
+                        costUSD,
+                        imgURL
+                    } = book
 
-                    const usdPrice = rank + 11;
-                    const convertedPrice = (getRate * usdPrice).toFixed(2);
+                    const usdPrice = costUSD;
+                    const convertedPrice = (getRate * costUSD).toFixed(2);
+
+                    async function handleOrderButton() {
+
+                    }
                     return (
-                        <div className="padding1 " key={rank} style={{
+                        <div className="padding1 " key={bookId} style={{
                             width: "350px", height: "620px", border: "1px solid white",
                             margin: "5%",
                             fontSize: "1rem",
@@ -111,28 +90,29 @@ const GetBooks: React.FC<Props> = ({ getRate }) => {
                             lineHeight: "1.8rem"
                         }}>
                             <div className="" >
+
                                 <div className="flex justify">
-                                    <img src={book_image} alt="book_image" style={{ minWidth: "310px", height: "310px" }} />
+                                    <img src={imgURL} alt="book_image" style={{ minWidth: "310px", height: "310px" }} />
                                 </div>
                                 <div className="justify flex" style={{ color: "aqua" }}>
-                                    {title}
+                                    {bookName}
                                 </div>
                                 <div className="justify flex">
                                     <hr />
                                 </div>
-                                <strong>Book rank: </strong>   &nbsp;{rank}
-                                <div className="">
-                                    <strong>Publisher </strong> :&nbsp;{publisher}
-                                </div>
+
                                 <div className="">
                                     <strong>author: </strong>      &nbsp; {author}
                                 </div>
                                 <div className="textoverflow">
-                                    <strong> Description: </strong>    &nbsp; {description}
+                                    <strong> Description: </strong>    &nbsp; {bookDescription}
                                 </div>
 
                                 <div className="">
-                                    <strong>primary_isbn13:  </strong>   &nbsp; {primary_isbn13}
+                                    <strong>Catagory:  </strong>   &nbsp; {bookCategory}
+                                </div>
+                                <div className="">
+                                    <strong>Quantity:  </strong>   &nbsp; {bookStockQuantity}
                                 </div>
 
                                 <div className="">
@@ -141,11 +121,10 @@ const GetBooks: React.FC<Props> = ({ getRate }) => {
                                 </div>
                                 <div className="">
                                     <strong>Converted Price: </strong>
-                                    &nbsp;$ <strong style={{ color: "gold" }}> {convertedPrice} </strong>
+                                    &nbsp; <strong style={{ color: "gold" }}> {convertedPrice} </strong>
                                     &nbsp;
-                                    <button className="btn btn-primary btn-sm">Add to Cart</button>
+                                    <button className="btn btn-primary btn-sm" onClick={handleOrderButton}>Add to Order</button>
                                 </div>
-
 
                             </div>
                         </div>
