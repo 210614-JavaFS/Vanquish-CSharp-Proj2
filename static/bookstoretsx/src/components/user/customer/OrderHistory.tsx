80601ecf3bd6992cc078d3a20fb8482d31bce500
JphanACC <69232559+JphanACC@users.noreply.github.com>
2021-08-15 15:21:02 -0600

    more stylized user profile, user order history and About us

--- static/bookstoretsx/src/components/user/customer/OrderHistory.tsx
+++ static/bookstoretsx/src/components/user/customer/OrderHistory.tsx
@@ -8,43 +8,51 @@ export default function OrderHistory(): JSX.Element {
         <div className="container-fluid">
             <div className="row">
                 <div className="col">
-                    <div className="d-flex justify-content-center">
-                        <h1 className="font-caps">Order History Page</h1>
+                    <div className="row ">
+                        <div className="col">
+                            <div className="d-flex justify-content-center">
+                                <h1 className="font-caps">Order History Page</h1>
+                            </div>
+                        </div>
                     </div>
-                    <div className="justify-content-center">
-                        <span>Username: </span>
-                        <span>{user.username}</span> <br/>
-                    </div>
-                </div>
-            </div>
 
-            <div className="row">
-                <table className="table">
-                    <thead>
-                        <tr className="font-caps">
-                            <th className="col-sm-1">Invoice ID</th>
-                            <th className="col-sm-1">Status</th>
-                            <th className="col-sm-1">USD Amount</th>
-                            <th className="col-sm-2">Native Currency</th>
-                            <th className="col-sm-2">Native Amount</th>
-                            <th className="col-sm-2">User</th>
-                            <th className="col-sm-3">Book's Name</th>
-                            <th className="col-sm-2"></th>
-                        </tr>
-                    </thead>
-                    <tbody className="black-txt arima-font">
-                        <tr>
-                            <td>{2}</td>
-                            <td>Pending</td>
-                            <td>53.22</td>
-                            <td>40.55</td>
-                            <td>EUR</td>
-                            <td>{user.username}</td>
-                            <td>The Lost Lamb</td>
-                            <td><button className="bttn-slant bttn-md bttn-danger bttn-no-outline">Cancel</button></td>
-                        </tr>
-                    </tbody>
-                </table>
+                    <div className="row modal-content padding-top-sm transparent-nav">
+                        <div className="justify-content-center modal-title cousine-font">
+                            <span>User:</span>
+                            <span className="padding-left-sm">{user.username}</span><br/>
+                        </div>
+                        <div className="justify-content-center modal-body">
+
+                            <table className="table">
+                                <thead>
+                                    <tr className="font-caps font-size-sm ">
+                                        <th className="col-sm-1">Invoice ID</th>
+                                        <th className="col-sm-1">Status</th>
+                                        <th className="col-sm-1">USD Amount</th>
+                                        <th className="col-sm-2">Native Currency</th>
+                                        <th className="col-sm-2">Native Amount</th>
+                                        <th className="col-sm-2">User</th>
+                                        <th className="col-sm-3">Book's Name</th>
+                                        <th className="col-sm-2"></th>
+                                    </tr>
+                                </thead>
+                                <tbody className="black-txt cousine-font">
+                                    <tr>
+                                        <td>{2}</td>
+                                        <td>Pending</td>
+                                        <td>53.22</td>
+                                        <td>40.55</td>
+                                        <td>EUR</td>
+                                        <td>{user.username}</td>
+                                        <td>The Lost Lamb</td>
+                                        <td><button className="bttn-slant bttn-md bttn-danger bttn-no-outline">Cancel</button></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
