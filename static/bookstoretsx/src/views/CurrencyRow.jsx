e269e2bf02cad1caf4e096ee80c2aac611392209
Munibk22 <Munib22@gmail.com>
2021-08-15 21:58:07 -0400

    front end changes

--- static/bookstoretsx/src/views/CurrencyRow.jsx
+++ static/bookstoretsx/src/views/CurrencyRow.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 // interface User {
 //     userId: number,
 //     username: string,
@@ -17,70 +19,71 @@ import React, { useEffect, useState } from 'react'
 //     user3: User
 // }
 
-export const CurrencyRow: React.FC<Props> = (props) => {
+export const CurrencyRow = (props) => {
+    const user = useSelector(selectUser);
     const {
-        currencies,
+        // currencies,
         selectedCurrency,
-        onChangeCUrrency,
+        // onChangeCUrrency,
         // amount,
-        onChangeAmount,
+        // onChangeAmount,
         value2,
         id,
         currencyName,
         user3,
         onChangeCurrency,
         symbol,
-        currencyOption,
-        selectCurrency
-
+        setConvertCurrency,
+        defaultUserCurrency
 
     } = props;
 
-    // const [currencyOption, setCurrencyOptions] = useState([]);
+    const [currencyOptions, setCurrencyOptions] = useState([]);
     // const [symbol, setSymbol] = useState("");
 
 
-    // useEffect(() => {
-    //     async function getCurrencyList() {
+    useEffect(() => {
+        async function getCurrencyList() {
+            const response = await axios.get("https://free.currconv.com/api/v7/currencies?apiKey=c728cb6404e7db7045e9");
 
-    //         const response = await axios.get("https://free.currconv.com/api/v7/currencies?apiKey=c728cb6404e7db7045e9");
-    //         console.log(response);
+            // const response = await axios.get("https://free.currconv.com/api/v7/countries?apiKey=c728cb6404e7db7045e9");
+            console.log(response);
 
-    //         if (response != null) {
-    //             let data = await response.data.results;
-    //             console.log(data[user3.currencyID]);
-    //             setSymbol(data[user3.currencyID])
-    //             setCurrencyOptions(Object.keys(data).map(function (key) {
-    //                 return data[key]
-    //             }))
-    //         }
-    //     }
-    //     console.log(currencyOption);
+            // if (response != null) {
+            let data = await response.data.results;
+            // console.log(data[user3.currencyID]);
+            // setSymbol(data[user3.currencyID])
+            setCurrencyOptions(Object.keys(data).map(function (key) {
+                return data[key]
+            }))
+            // }
+        }
+        currencyOptions ? console.log(currencyOptions) : console.log("");;
 
-    //     getCurrencyList();
+        getCurrencyList();
 
-    //     return () => {
+        return () => {
 
-    //     }
-    // }, [])
+        }
+    }, [])
 
     // const onChangeCurrency = (e) => {
 
     // }
 
 
-    console.log(selectCurrency);
+    console.log(selectedCurrency);
 
-    console.log(user3.currencyID);
+    console.log(user.currencyID);
     return (
         <div className="flex " style={{ justifyContent: "flex-end" }}>
             <div className="">
                 <h3>Currency List</h3>
 
-                {/* value={selectCurrency} */}
-                <select style={{ width: "300px" }} onChange={onChangeCurrency}>
+                {/* value={setConvertCurrency} */}
+                <select style={{ width: "300px" }} value={selectedCurrency} onChange={onChangeCurrency}>
 
-                    {currencyOption.map((option) => {
+                    {currencyOptions.map((option) => {
                         const {
                             currencyName,
                             id
